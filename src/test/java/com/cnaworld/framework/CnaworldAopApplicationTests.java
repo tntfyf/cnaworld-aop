5c3767fb7e11fdcec9df66e000980521f905ea70
Lucifer <tntfyf@yeah.net>
2023-01-31 01:41:43 +0800

    提交1.0实现

---
+++ src/test/java/com/cnaworld/framework/CnaworldAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnaworld.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnaworldAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
